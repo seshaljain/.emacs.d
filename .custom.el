377c77ccc99444a1f03b836605fc5b1595565b7e
Seshal Jain <seshaljain11@gmail.com>
2020-07-25 10:54:02 +0530

    Initial commit

---
+++ .custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("dcdd1471fde79899ae47152d090e3551b889edf4b46f00df36d653adc2bf550d" default)))
+ '(org-directory "~/Dropbox/org")
+ '(org-startup-folded (quote overview))
+ '(package-selected-packages (quote (dracula-theme exec-path-from-shell use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
