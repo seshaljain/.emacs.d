76205cbccaee9c4c002ca869c193de4fec2db55b
Seshal Jain <seshaljain11@gmail.com>
2020-08-21 22:14:04 +0530

    Remove redundant custom config

--- .custom.el
+++ .custom.el
@@ -3,14 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("dcdd1471fde79899ae47152d090e3551b889edf4b46f00df36d653adc2bf550d" default)))
- '(org-directory "~/Dropbox/org")
- '(org-startup-folded (quote overview))
  '(package-selected-packages
    (quote
-    (lorem-ipsum markdown-mode iedit dracula-theme dracula yasnippet-snippets which-key web-mode use-package try rjsx-mode rainbow-mode prettier magit expand-region exec-path-from-shell emmet-mode diminish counsel auto-complete ace-jump-mode))))
+    (jedi dracula-theme yasnippet-snippets which-key web-mode use-package try rjsx-mode rainbow-mode prettier mode-line-bell markdown-mode magit lorem-ipsum iedit expand-region exec-path-from-shell emmet-mode diminish counsel auto-complete ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
