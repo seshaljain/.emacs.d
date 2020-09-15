701f1849e551129cf1bd5ca08502dfd633691acf
Seshal Jain <seshaljain11@gmail.com>
2020-09-15 10:01:34 +0530

    Package: projectile

--- .custom.el
+++ .custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(pyvenv dracula-theme yasnippet-snippets which-key web-mode use-package try rjsx-mode rainbow-mode prettier nyan-mode mode-line-bell markdown-mode magit lorem-ipsum iedit expand-region exec-path-from-shell emmet-mode eglot diminish counsel company ace-jump-mode)))
+   '(counsel-projectile projectile pyvenv dracula-theme yasnippet-snippets which-key web-mode use-package try rjsx-mode rainbow-mode prettier nyan-mode mode-line-bell markdown-mode magit lorem-ipsum iedit expand-region exec-path-from-shell emmet-mode eglot diminish counsel company ace-jump-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
